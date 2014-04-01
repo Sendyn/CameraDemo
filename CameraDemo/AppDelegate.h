8f40a87ce2e2ef0748f2107b6573f94457bb499c
apple <taagoomacmini@apple.local>
2014-04-01 10:46:21 +0800

    Initial Commit

---
+++ CameraDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CameraDemo
+//
+//  Created by apple on 14-4-1.
+//  Copyright (c) 2014年 Taagoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CameraDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CameraDemo
+//
+//  Created by apple on 14-4-1.
+//  Copyright (c) 2014年 Taagoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
